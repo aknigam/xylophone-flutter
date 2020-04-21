4545f359eaa2c3b964b71cba0c5c5a32aabe9368
Nigam, Anand <a.nigam@cvent.com>
2020-04-21 08:41:10 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound({int track}) {
+    final player = AudioCache();
+    player.play('note$track.wav');
+  }
+
+  Widget buildKey({int track, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(track: track);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(track: 1, color: Colors.red),
+              buildKey(track: 2, color: Colors.green),
+              buildKey(track: 3, color: Colors.blue),
+              buildKey(track: 4, color: Colors.yellow),
+              buildKey(track: 5, color: Colors.orange),
+              buildKey(track: 6, color: Colors.pink),
+              buildKey(track: 7, color: Colors.amber),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
